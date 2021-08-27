8fa772425528eeb04c925077a6fb327c9074da61
Andrei Santos <andrei.as3@hotmail.com>
2021-08-26 22:14:35 -0300

    create cli e server web

---
+++ adapters/web/handler/product.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/andreis3/go-hexagonal/application"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServideInterface) {
+	r.Handle("/product/{id}", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods("GET", "OPTIONS")
+}
+
+func getProduct(service application.ProductServideInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		err = json.NewEncoder(w).Encode(product)
+		fmt.Println(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.16
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/codegangsta/negroni v1.0.0
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/golang/mock v1.6.0
+	github.com/gorilla/mux v1.8.0
 	github.com/mattn/go-sqlite3 v1.14.8
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/cast v1.4.1 // indirect
